2adf34db86adb6c035fad724a4779af55435bf39
Prithviraj Deshmane <prithvirajdeshmane@gmail.com>
2024-05-20 10:17:28 -0700

    add screen listeners for arrow key events, snake heading responds to arrow keys

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from turtle import Screen
 import time
 from snake import Snake
 
+REFRESH_RATE_IN_SECONDS = 0.2
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
 # Crete the screen
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 screen.bgcolor("black")
 screen.title("The Snake Game")
 screen.tracer(0)
@@ -12,12 +16,19 @@ screen.tracer(0)
 # create a new snake object instance
 snake = Snake()
 
+# start screen listener, and listen for arraw keys
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 # Track if game is on
 is_game_on = True
 
 while is_game_on:
     screen.update()
-    time.sleep(1)
+    time.sleep(REFRESH_RATE_IN_SECONDS)
     snake.move()
 
 # Close the screen
--- snake.py
+++ snake.py
@@ -4,6 +4,12 @@ from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 
+# Set directional heading angles
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 
 class Snake:
     def __init__(self):
@@ -11,7 +17,7 @@ class Snake:
         # List of the snake body segments
         self.snake_segments = []
         self.create_snake()
-        head = self.snake_segments[0]
+        self.head = self.snake_segments[0]
 
     def create_snake(self):
         """ create the body of the snake """
@@ -34,3 +40,23 @@ class Snake:
             new_y = self.snake_segments[i - 1].ycor()
             self.snake_segments[i].goto(new_x, new_y)
         self.snake_segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        """ Turns the snake in the UP (North) direction, as long as current heading is not DOWN """
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        """ Turns the snake in the DOWN (South) direction, as long as current heading is not UP """
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        """ Turns the snake LEFT (West), as long as current heading is not RIGHT """
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        """ Turns the snake RIGHT (East), as long as current heading is not LEFT """
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
