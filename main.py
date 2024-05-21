6a11152278c030d804d7fd8db584431c35d95255
Prithviraj Deshmane <prithvirajdeshmane@gmail.com>
2024-05-21 10:39:20 -0700

    detect collision of snake with its own body, refactor an  end_game method

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 
-REFRESH_RATE_IN_SECONDS = 0.2
+REFRESH_RATE_IN_SECONDS = 0.15
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 BOUNDARY_LIMIT = 290
@@ -35,6 +35,15 @@ screen.onkey(snake.right, "Right")
 # Track if game is on
 is_game_on = True
 
+
+# set the game end status
+def end_game():
+    """ sets the game end status on screen and for the game """
+    scoreboard.game_over()
+    global is_game_on
+    is_game_on = False
+
+
 while is_game_on:
     screen.update()
     scoreboard.show_score()
@@ -52,8 +61,13 @@ while is_game_on:
             or snake.head.xcor() > BOUNDARY_LIMIT \
             or snake.head.ycor() < -1 * BOUNDARY_LIMIT \
             or snake.head.ycor() > BOUNDARY_LIMIT:
-        scoreboard.game_over()
-        is_game_on = False
+        end_game()
+
+    # detect collision with tail
+    for segment in snake.snake_segments:
+        if segment != snake.head:
+            if snake.head.distance(segment) < 10:
+                end_game()
 
 # Close the screen
 screen.exitonclick()
